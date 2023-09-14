34673b618551651352cae870d0c0ba20b483fe52
muni <booscarface@gmail.com>
2023-09-14 13:02:10 +0200

    added notice

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function run() {
     const s3Uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
+    core.notice('Hello from my custom Javascript Action!')
 
 
 }
